d368cd9adb87b6b101879a17daa9d5ed99bdd3b5
phillip <phillip@redpill.local>
2025-01-07 11:34:38 -0800

    Initial commit

---
+++ Polygon/Square.java
@@ -0,0 +1,10 @@
+public class Square extends Rectangle{
+    public Square(double side) {
+        super(side, side); // A square has equal length and width
+    }
+
+    @Override
+    public void describe() {
+        System.out.println("I am a square with area " + super.calculateArea() + ".");
+    }
+}
